5633c3788bc2f1c9aa3466dded169e5d9903937f
Porter Jones <pbjones989@gmail.com>
2024-12-29 15:47:48 +0000

    Add initial bashrc

--- setup.sh
+++ setup.sh
@@ -8,10 +8,17 @@ _link_config_dir() {
 	ln -sf "${PWD}/${target_dirname}" "${CONFIG_DIR}"
 }
 
+# i3 configs
 _link_config_dir "i3"
 _link_config_dir "i3status"
+
+
+# x server configs
 _link_config_dir "Xresources.d"
 
 ln -sf "${PWD}/.Xresources" "${HOME}"
 
 sudo ln -sf "${PWD}/xorg.conf.d"/* "/etc/X11/xorg.conf.d/"
+
+# bash configs
+ln -sf "${PWD}/.bashrc" "${HOME}"
