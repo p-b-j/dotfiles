ee9f7b69b919b50431d09e5c7f68f19e5d0000df
Porter Jones <pbjones989@gmail.com>
2024-12-29 11:30:16 +0000

    Add script for linking dotfiles

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CONFIG_DIR="${HOME}/.config"
+
+_link_config_dir() {
+	target_dirname="$1"
+
+	ln -sf "${PWD}/${target_dirname}" "${CONFIG_DIR}"
+}
+
+_link_config_dir "i3"
+_link_config_dir "i3status"
+_link_config_dir "Xresources.d"
+
+ln -sf "${PWD}/.Xresources" "${HOME}"
