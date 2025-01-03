e6cb5c4c6ea488f93f84a2b40624be7f4b0ba5b9
Porter Jones <pbjones989@gmail.com>
2025-01-03 19:55:19 +0000

    nvim everforest: darker background

--- nvim/lua/color_config.lua
+++ nvim/lua/color_config.lua
@@ -11,5 +11,12 @@ pkg_manager.add_pkgs({
 
 pkg_manager.add_setup_fn(function()
     vim.opt.termguicolors = true
-    vim.cmd.colorscheme("everforest")
+    local everforest = require("everforest")
+    everforest.setup({
+        background = "hard",
+        colours_override = function(palette)
+            palette.background = "#232a2e"
+        end,
+    })
+    everforest.load()
 end)
