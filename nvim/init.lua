26e1f606991ae4f9211ea3861e81c8f94a069220
Porter Jones <pbjones989@gmail.com>
2025-01-03 18:40:57 +0000

    Tweak nvim telescope configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,7 @@ require("color_config")
 require("pkg_manager").add_pkgs({
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
     },
 })
 
--- nvim/lua/general_config.lua
+++ nvim/lua/general_config.lua
@@ -1,2 +1,5 @@
 vim.opt.signcolumn = "yes"
 vim.g.mapleader = "g"
+
+vim.wo.number = true
+vim.wo.relativenumber = true
