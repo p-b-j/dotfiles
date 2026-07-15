1c57bae0fed63ccd4d9e00bfddf194b750b7189e
Porter Jones <porter@mechanical-orchard.com>
2026-07-15 15:35:58 +0100

    Add gitsigns plugin

--- dot.config/nvim/lua/plugins/lazy/packages.lua
+++ dot.config/nvim/lua/plugins/lazy/packages.lua
@@ -1,8 +1,9 @@
 return {
-    { "tpope/vim-commentary" },
-    { "tpope/vim-sleuth" },
-    { "tpope/vim-fugitive" },
     { "elixir-editors/vim-elixir" },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/nvim-cmp" },
+    { "lewis6991/gitsigns.nvim" },
+    { "tpope/vim-commentary" },
+    { "tpope/vim-sleuth" },
+    { "tpope/vim-fugitive" },
 }
